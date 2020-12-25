bba15e7c3fd937c082ee5f8f2e4db939455fff0b
Shuo-En Li <sli112@ucsc.edu>
2020-12-25 14:31:56 -0800

    [13] Add functionality to send message to myself through the website

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
+import requests
+import json
 from dotenv import load_dotenv, find_dotenv
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template, redirect, url_for, Response
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -20,6 +22,47 @@ line_bot_api = LineBotApi(channel_access_token)
 # Channel Secret
 channel_secret = os.getenv('CHANNEL_SECRET')
 handler = WebhookHandler(channel_secret)
+
+permitted = False
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        password = request.form['password']
+        if password != '123456':
+            return "You don't have access to this page"
+    if request.method == 'GET':
+        return render_template('home.html')
+    global permitted
+    permitted = True
+    return redirect(url_for("main"))
+
+
+@app.route("/main", methods=['GET', 'POST'])
+def main():
+    global permitted
+    if not permitted:
+        return "You cannot directly access this page!"
+    if request.method == 'POST':
+        message = request.form['message-context']
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {channel_access_token}'
+        }
+        data = {
+            "to": "U68224c4538a68ae95239bb8a15807a1c",
+            "messages": [{"type": "text", "text": message}]
+        }
+        try:
+            r = requests.post(url='https://api.line.me/v2/bot/message/push', headers=headers, json=data)
+            r.raise_for_status()
+        except requests.RequestException as e:
+            raise SystemExit(e)
+        else:
+            return "SUCCESS. Message sent: " + message
+
+    return render_template('main.html')
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -46,8 +89,6 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="hello there!"))
     elif text == "bye":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="bye :("))
-
-
 
 
 if __name__ == "__main__":
