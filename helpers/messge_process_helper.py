c9efa8296d0beda09d8bef1a20753e4f84f17e45
Shuo-En Li <sli112@ucsc.edu>
2020-12-26 08:37:37 -0800

    [16] Add support for English and add test coverage for it

--- helpers/messge_process_helper.py
+++ helpers/messge_process_helper.py
@@ -1,22 +1,66 @@
 class MessageProcessHelper:
-    random_replies_list = [
-        "你真的很可愛耶！哈哈哈哈哈",
-        "沒什麼事的話我先去玩沙囉，疑不對，魟魚會玩沙嗎？",
-        "有感恩的事嗎？沒有的話我先去玩沙囉！",
-        "我聽著呢！",
-        "其實我也沒有厲害到聽得懂你說的話啦...哈哈",
-        "我要去跟我的貝殼玩了～薩呦那拉",
-        "魟魚我今天感恩的事情是什麼事不用做耍廢了一天",
-        "咕嚕咕嚕咕嚕嚕",
-        "蛤？你說什麼！"
-    ]
+    random_replies = {
+        "zh-cn": [
+            "你真的很可愛耶！哈哈哈哈哈",
+            "沒什麼事的話我先去玩沙囉，疑不對，魟魚會玩沙嗎？",
+            "有感恩的事嗎？沒有的話我先去玩沙囉！",
+            "我聽著呢！",
+            "其實我也沒有厲害到聽得懂你說的話啦...哈哈",
+            "我要去跟我的貝殼玩了～薩呦那拉",
+            "魟魚我今天感恩的事情是什麼事不用做耍廢了一天",
+            "咕嚕咕嚕咕嚕嚕",
+            "蛤？你說什麼！",
+            "能做你朋友我很榮幸，你帶給我好多快樂哦！",
+            "你好好玩喔ＸＤ"
+        ],
+        "en": [
+            "You are such a cutie :D",
+            "Just chilling, I guess",
+            "Any thankful things? If not I'm gonna go play with crab now!",
+            "Go on, I am listening",
+            "To be totally honest I don't understand what you are saying",
+            "Gotta go play with them sea stars now, see-ya",
+            "The thing I am thankful for is I can be a sea couch potato",
+            "goo-loo-goo-loo",
+            "Huh? What?",
+            "I am glad we are friends :) You bring me so much happiness!",
+            "You are so amusing XD"
+        ],
+    }
+
+    thank_keywords = {
+        "zh-cn": ["感恩", "感謝", "謝恩", "謝謝"],
+        "en": ["thankful", "grateful"]
+    }
+
+    thank_reply = {
+        "zh-cn": f"確實是很值得感恩的事呢！「{0}」，幫你記錄下來囉:)",
+        "en": f"'{0}'!Something to be thankful for indeed! I have noted it down for you :)"
+    }
 
     def __init__(self, user_input):
         self.user_input = user_input
 
+    def detect_language(self, text) -> str:
+        from langdetect import detect
+        lan = detect(text)
+        # if the detected language is not English nor Chinese
+        if lan not in ['zh-cn', 'en']:
+            # Correct the common mistake
+            if lan == 'ko':
+                return 'zh-cn'
+            if lan == 'ro':
+                return 'en'
+        return lan
+
     def calc_reply(self) -> str:
         from random import choice
-        if '感恩' in self.user_input:
-            return f"確實是很值得感恩的事呢！「{self.user_input}」，幫你記錄下來囉:)"
+        lan = self.detect_language(self.user_input)
+
+        # If the user input contains the thankful keyword for that language,
+        # then reply with the thankful reply
+        # Else, give a randomized reply of the language
+        if any(k in self.user_input for k in self.thank_keywords[lan]):
+            return self.thank_reply[lan].format(self.user_input)
         else:
-            return choice(self.random_replies_list)
+            return choice(self.random_replies[lan])
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -3,13 +3,7 @@ from helpers.messge_process_helper import MessageProcessHelper
 
 class TestMessage:
 
-    def test_grateful_reply(self):
-        user_input = "我今天要感恩的事情是～"
-        reply = MessageProcessHelper(user_input).calc_reply()
-        print(reply)
-        assert "確實" in reply
-
-    def test_randomized_reply(self):
+    def test_random_replies(self):
         user_input = "真的喔？"
         replies = []
         for _ in range(6):
@@ -17,3 +11,34 @@ class TestMessage:
             replies.append(reply)
 
         assert len(set(replies)) > 3
+
+    def test_language_input_thankful(self):
+        user_input = "好感謝今天..."
+        reply = MessageProcessHelper(user_input).calc_reply()
+        assert "確實" in reply
+
+        user_input = "真的很感恩今天..."
+        reply = MessageProcessHelper(user_input).calc_reply()
+        assert "確實" in reply
+
+        user_input = "I am thankful for ...."
+        reply = MessageProcessHelper(user_input).calc_reply()
+        assert "indeed" in reply
+
+        user_input = "I am grateful for..."
+        reply = MessageProcessHelper(user_input).calc_reply()
+        assert "indeed" in reply
+
+    def test_language_input_random(self):
+        from langdetect import detect
+
+        user_input = "是喔"
+        mph = MessageProcessHelper(user_input)
+        reply = mph.calc_reply()
+        assert mph.detect_language(reply) == 'zh-cn'
+
+        user_input = "Oh really?"
+        mph = MessageProcessHelper(user_input)
+        reply = mph.calc_reply()
+        assert mph.detect_language(reply) == 'en'
+
